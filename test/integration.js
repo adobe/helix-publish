7db602cab4cd856dcf888f999ba287c2872e7abe
Tobias Bocanegra <tripod@adobe.com>
2019-07-24 16:52:49 +0900

    test(integration): adjust to new pingdom status

--- test/integration.js
+++ test/integration.js
@@ -188,7 +188,7 @@ describe('Integration Test', () => {
       });
     }
 
-    const res = await main({ __ow_method: 'get' });
+    const res = await main({ __ow_method: 'get', __ow_path: '/_status_check/pingdom.xml' });
 
     assert.equal(res.statusCode, 200);
     assert.ok(res.body.match(/<status>OK<\/status>/));
