6ffda450a10e748d7ecc722f2227d93a93f8353a
Tobias Bocanegra <tripod@adobe.com>
2019-08-19 16:32:39 -0700

    chore(deps): use action-utils

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-const { logger: setupLogger } = require('@adobe/openwhisk-action-builder/src/logging');
+const { logger: setupLogger } = require('@adobe/openwhisk-action-utils');
 const { wrap } = require('@adobe/helix-pingdom-status');
 const publish = require('./src/publish');
 
