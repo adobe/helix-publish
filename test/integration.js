ca0f723931de234da8bc88ee35b637bd653aadb6
Tobias Bocanegra <tripod@adobe.com>
2020-08-25 17:21:42 +0900

    chore: fix test

--- test/integration.js
+++ test/integration.js
@@ -200,10 +200,10 @@ describe('Integration Test', () => {
       this.polly.server.any('https://api.fastly.com/*').intercept((req, res) => {
         res.sendStatus(500);
       });
-      this.polly.server
-        .get('https://adobeioruntime.net/api/v1/web/mrosier/9d723ce487448cc132cd240a484b65772b201241/html/_status_check/healthcheck.json')
-        .intercept((req, res) => res.json({ status: 'OK', version: '1.2.3' }));
     }
+    this.polly.server
+      .get('https://adobeioruntime.net/api/v1/web/mrosier/9d723ce487448cc132cd240a484b65772b201241/html/_status_check/healthcheck.json')
+      .intercept((req, res) => res.json({ status: 'OK', version: '1.2.3' }));
 
     const params = {
       service: HLX_FASTLY_NAMESPACE,
@@ -221,10 +221,10 @@ describe('Integration Test', () => {
       this.polly.server.any('https://api.fastly.com/service/54nWWFJicKgbdVHou26Y6a/version/247/backend/AdobeFonts').intercept((req, res) => {
         res.sendStatus(200);
       });
-      this.polly.server
-        .get('https://adobeioruntime.net/api/v1/web/mrosier/9d723ce487448cc132cd240a484b65772b201241/html/_status_check/healthcheck.json')
-        .intercept((req, res) => res.json({ status: 'OK', version: '1.2.3' }));
     }
+    this.polly.server
+      .get('https://adobeioruntime.net/api/v1/web/mrosier/9d723ce487448cc132cd240a484b65772b201241/html/_status_check/healthcheck.json')
+      .intercept((req, res) => res.json({ status: 'OK', version: '1.2.3' }));
 
     const params = {
       service: HLX_FASTLY_NAMESPACE,
