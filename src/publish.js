fd6bdc7a0b08c353df8c08fc8f845cc92498db49
Lars Trieloff <trieloff@adobe.com>
2020-10-06 14:54:49 +0000

    fix(publish): reduce parallelism in updating vcl
    otherwise the fastly api fails
    
    fixes #628

--- src/publish.js
+++ src/publish.js
@@ -82,6 +82,14 @@ async function publish(configuration, service, token, version, vclOverrides = {}
     await checkPkgs(config, log);
     const fastly = await initfastly(token, service);
     log.info('running publishing tasks...');
+
+    let pretasks = 1;
+    await vcl.dynamic(fastly, version, dispatchVersion);
+    await vcl.extensions(fastly, version, vclOverrides);
+    await vcl.updatestrains(fastly, version, config.strains);
+    await vcl.queries(fastly, version, indexconfig);
+    pretasks += 4;
+
     const publishtasks = [
       backends.init(fastly, version, algoliaappid),
       backends.updatestrains(fastly, version, config.strains),
@@ -90,10 +98,6 @@ async function publish(configuration, service, token, version, vclOverrides = {}
           token: epsagonToken, logname: 'helix-epsagon', serviceid: service, epsagonAppName,
         }
         : undefined),
-      vcl.dynamic(fastly, version, dispatchVersion),
-      vcl.extensions(fastly, version, vclOverrides),
-      vcl.updatestrains(fastly, version, config.strains),
-      vcl.queries(fastly, version, indexconfig),
       redirects.updatestrains(fastly, version, config.strains),
       dictionaries.init(
         fastly,
@@ -111,11 +115,11 @@ async function publish(configuration, service, token, version, vclOverrides = {}
     return Promise.all(publishtasks)
       .then(() => vcl.finish(fastly, version))
       .then(() => {
-        log.info(`completed ${publishtasks.length + 1} tasks.`);
+        log.info(`completed ${publishtasks.length + pretasks} tasks.`);
         return {
           body: {
             status: 'published',
-            completed: publishtasks.length + 1,
+            completed: publishtasks.length + pretasks,
           },
           statusCode: 200,
         };
